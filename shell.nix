a8f8bfdcc36be51e2d999dd01a959a9e046bad06
tanaxer01 <jpbrione@gmail.com>
2023-05-03 15:12:03 -0400

    a

--- shell.nix
+++ shell.nix
@@ -1,50 +1,46 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-21.11.tar.gz") { } }:
 
 let
-    kapack = import
-        (fetchTarball "https://github.com/oar-team/nur-kapack/archive/master.tar.gz") {};
+  kapack = import (fetchTarball "https://github.com/oar-team/nur-kapack/archive/master.tar.gz") {};
+
+  customPackages = pkgs: kapack: rec {
+    my-batsim = kapack.batsim-410.overrideAttrs(old: rec {
+      version = "3e7a7e5ecf1b749c306c532f828219e5a3d70862";
+      src = kapack.pkgs.fetchgit rec {
+        url = "https://framagit.org/batsim/batsim.git";
+        rev = version;
+        sha256 = "1f3xij13i4wgd667n0sqdkkzn5b11fz4bmxv9s0yivdniwn6md76";
+      };
+    });
+
+    my-batsched = kapack.batsched-140.overrideAttrs(old: rec {
+      version = "b020e48b89a675ae681eb5bcead01035405b571e";
+      src = kapack.pkgs.fetchgit rec {
+        url = "https://framagit.org/batsim/batsched.git";
+        rev = version;
+        sha256 = "1dmx2zk25y24z3m92bsfndvvgmdg4wy2iildjmwr3drmw15s75q0";
+      };
+    });
+
+    my-pybatsim = kapack.pybatsim-320.overridePythonAttrs(old: rec {
+      version = "fa6600eccd4e5ffbebfc7ecba05b64cf84c5e9d3";
+      src = kapack.pkgs.fetchgit rec {
+        url = "https://gitlab.inria.fr/batsim/pybatsim.git";
+        rev = version;
+        sha256 = "0nr8hqsx6y1k0lv82f4x3fjq6pz2fsd3h44cvnm7w21g4v3s6l6y";
+      };
+    });
+  };
 in
 
-let my-packages = rec {
-    my-batsim = kapack.batsim-410.overrideAttrs(old: rec {
-        version = ""
-        src = kapack.pkgs.fetchgit rec {
-            url = "https://framagit.org/batsim/batsim.git";
-            rev = version;
-            sha256 = "";
-        };
-    });
-
-    my-batsched = kapack.batshed-140.overrideAttrs(old: rec {
-        version = ""
-        src = kapack.pkgs.fetchgit rec {
-            url = "https://framagit.org/batsim/batshed.git";
-            rev = version;
-            sha256 = "";
-        };
-    });
-
-    my-pybatsim = kapack.batshed-140.overrideAttrs(old: rec {
-        version = ""
-        src = kapack.pkgs.fetchgit rec {
-            url = "https://gitlab.inria.fr/batsim/pybatsim.git";
-            rev = version;
-            sha256 = "";
-        };
-    });
-}
-
-
-
 pkgs.mkShell {
-  # nativeBuildInputs is usually what you want -- tools you need to run
   nativeBuildInputs = with pkgs; [
     nixpkgs-fmt
     rnix-lsp
     docker-client
     gnumake
     git
-    rust
+    go
   ];
 
   buildInputs = with (customPackages pkgs kapack); [
