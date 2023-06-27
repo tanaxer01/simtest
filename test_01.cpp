d8305652b4ccfd5fbdd42751d692e771a25c3131
tanaxer01 <jpbrione@gmail.com>
2023-06-27 12:07:46 -0400

    update

--- test_01.cpp
+++ test_01.cpp
@@ -1,42 +1,38 @@
-#include "test_01.hpp"
+#include <simgrid/s4u.hpp>
+
+namespace sg4 = simgrid::s4u;
 
 XBT_LOG_NEW_DEFAULT_CATEGORY(s4u_test, "Messages specific for this s4u example");
 
-Master::Master(std::string dag_file) 
-{
-  XBT_INFO("Starting master class.");
-  dag = sg4::create_DAG_from_dot(dag_file);
 
-}
-
-void Master::operator()() 
-{
-  int count = 1; 
-  for (const auto& host : sg4::Engine::get_instance()->get_all_hosts()) {
-    sg4::Actor::create("worker" + std::to_string(count++), host, &worker);
-  }
-}
-
-static void worker()
-{
-  XBT_INFO("Creating worker func at %s.", sg4::this_actor::get_host()->get_cname());
-
-}
-
-int main(int argc, char** argv) 
+int main(int argc, char* argv[])
 {
   sg4::Engine e(&argc, argv);
-  xbt_assert(argc > 2, "Usage: %s plataform_file.xml dag_file.dot\n", argv[0]);
+  xbt_assert(argc > 2, "Usage: %s platform_file deployment_file\n", argv[0]);
 
-  /* Load the plataform description */
+  /* Load the platform description and the dag for the applicationand the dag for the application  */
   e.load_platform(argv[1]);
 
-  /* Initial scheduling */
-  // TODO: Find "best" host to asign root
-  sg4::ActorPtr root = sg4::Actor::create("master", e.host_by_name("Tremblay"), Master(argv[2]));
+  std::vector<sg4::ActivityPtr> dag = sg4::create_DAG_from_DAX(argv[2]);
 
-  /* Run the simulation */ 
+  sg4::Host* tremblay = e.host_by_name("Tremblay");
+  sg4::Host* jupiter = e.host_by_name("Jupiter");
+
+  dynamic_cast<sg4::Exec*>(dag[0].get())->set_host(tremblay);
+  dynamic_cast<sg4::Exec*>(dag[1].get())->set_host(jupiter);
+
+  sg4::Activity::on_completion_cb([](const sg4::Activity& a) {
+    bool is_exec = dynamic_cast<sg4::Exec const*>(&a) != nullptr; 
+
+    XBT_INFO("%s '%s' is complete (start time: %f, finish time: %f)", 
+      (is_exec)? "Exec" : "Comms",
+      a.get_cname(),
+      a.get_start_time(), a.get_finish_time());
+  });
+
+  
+
+
   e.run();
-
   return 0;
 }
--- test_02.cpp
+++ test_02.cpp
@@ -1,80 +1,93 @@
 #include "simgrid/s4u.hpp"
-#include <vector>
 
-XBT_LOG_NEW_DEFAULT_CATEGORY(s4u_test, "Messages specific for this s4u example");
+#include <stdio.h>
+#include <string.h>
+
+XBT_LOG_NEW_DEFAULT_CATEGORY(dag_from_dax, "Logging specific to this example");
 namespace sg4 = simgrid::s4u;
 
-int main(int argc, char* argv[])
+
+int main(int argc, char** argv)
 {
-  sg4::Engine e(&argc, argv);
-  e.load_platform(argv[1]);
-  std::set<sg4::Activity*> vetoed;
-  e.track_vetoed_activities(&vetoed);
+    sg4::Engine e(&argc, argv);
+    e.load_platform(argv[1]);
 
-  auto fafard = e.host_by_name("Fafard");
+    std::vector<sg4::ActivityPtr> dag = sg4::create_DAG_from_DAX(argv[2]);
 
-  // Display the details on vetoed activities
-  sg4::Activity::on_veto_cb([](const sg4::Activity& a) {
-    const auto& exec = static_cast<const sg4::Exec&>(a); // all activities are execs in this example
-
-    XBT_INFO("Activity '%s' vetoed. Dependencies: %s; Ressources: %s", exec.get_cname(),
-             (exec.dependencies_solved() ? "solved" : "NOT solved"),
-             (exec.is_assigned() ? "assigned" : "NOT assigned"));
-  });
-
-  sg4::Activity::on_completion_cb([](sg4::Activity const& activity) {
-    const auto* exec = dynamic_cast<sg4::Exec const*>(&activity);
-    if (exec == nullptr) // Only Execs are concerned here
-      return;
-    XBT_INFO("Activity '%s' is complete (start time: %f, finish time: %f)", exec->get_cname(), exec->get_start_time(),
-             exec->get_finish_time());
-  });
-
-  // Define an amount of work that should take 1 second to execute.
-  double computation_amount = fafard->get_speed();
-
-  // Create a small DAG: Two parents and a child
-  sg4::ExecPtr A = sg4::Exec::init();
-  sg4::ExecPtr B = sg4::Exec::init();
-  sg4::ExecPtr C = sg4::Exec::init();
-
-  A->add_successor(B);
-  B->add_successor(C);
-
-  // Set the parameters (the name is for logging purposes only)
-  // + First parent ends after 1 second and the Second parent after 2 seconds.
-  A->set_name("A")->set_flops_amount(computation_amount);
-  B->set_name("B")->set_flops_amount(5 * computation_amount);
-  C->set_name("C")->set_flops_amount(computation_amount);
-
-  // Only the parents are scheduled so far
-  A->set_host(fafard);
-  B->set_host(fafard);
-  C->set_host(fafard);
-
-  // Start all activities that can actually start.
-  A->start();
-  B->start();
-  C->start();
-
-  while (C->get_state() != sg4::Activity::State::FINISHED) {
-    e.run();
-
-    for (auto* a : vetoed) {
-      auto* exec = static_cast<sg4::Exec*>(a);
-
-      // In this simple case, we just assign the child task to a resource when its dependencies are solved
-      if (exec->dependencies_solved() && not exec->is_assigned()) {
-        XBT_INFO("Activity %s's dependencies are resolved. Let's assign it to Fafard.", exec->get_cname());
-        exec->set_host(fafard);
-      } else {
-        XBT_INFO("Activity %s not ready.", exec->get_cname());
-      }
+    if (dag.empty()) {
+        XBT_ERROR("A problem occurred during DAX parsing (cycle or syntax). Do not continue this test");
+        exit(2);
     }
-    vetoed.clear(); // DON'T FORGET TO CLEAR this set between two calls to run
-  }
 
-  XBT_INFO("Simulation time %g", sg4::Engine::get_clock());
 
-  return 0;
+    XBT_INFO("--------- Display all activities of the loaded DAG -----------");
+    for (const auto& a : dag) {
+        if (dynamic_cast<sg4::Comm*>(a.get()) != nullptr)
+            continue;
+
+        XBT_INFO("Exec %s %.0f flops to execute | Dependencies %s; Resources: %s", a->get_cname(), a->get_remaining(), 
+                (a->dependencies_solved() ? "solved" : "NOT solved"),
+                (a->is_assigned() ? "assigned" : "NOT assigned"));
+    }
+
+    XBT_INFO("------------------- Schedule tasks ---------------------------");
+    auto hosts = e.get_all_hosts();
+    auto count = e.get_host_count();
+    int cursor = 0;
+    // Schedule end first
+    static_cast<sg4::Exec*>(dag.back().get())->set_host(hosts[0]);
+
+    for (const auto& a : dag) {
+        if (auto* exec = dynamic_cast<sg4::Exec*>(a.get()); exec != nullptr && exec->get_name() != "end") {
+            exec->set_host(hosts[cursor % count]);
+            cursor++;
+        }
+
+        if (auto* comm = dynamic_cast<sg4::Comm*>(a.get())) {
+            const auto* pred = dynamic_cast<sg4::Exec*>((*comm->get_dependencies().begin()).get());
+            const auto* succ = dynamic_cast<sg4::Exec*>(comm->get_successors().front().get());
+            comm->set_source(pred->get_host())->set_destination(succ->get_host());
+        }
+    }
+
+    XBT_INFO("------------------- Run the schedule -------------------------");
+    double next_step = 0;
+    double STEP = 1000;
+
+    while (dag.back()->get_state() != sg4::Activity::State::FINISHED) {
+        e.run_until(next_step);
+        next_step += STEP;
+        
+        XBT_INFO("---- STEP %f | %f ----", next_step, e.get_clock());
+        for (const auto& a : dag) {
+            auto *exec = dynamic_cast<sg4::Exec*>(a.get());
+            if (exec == nullptr)
+                continue;
+
+            if(next_step > 7000 && exec->get_name() == "1@task1" && exec->get_state() != sg4::Activity::State::FINISHED)
+                exec->set_host(sg4::Host::by_name("Jupiter"));
+
+            XBT_INFO("%s - %s; %s %f", exec->get_cname(), exec->get_host()->get_cname(), exec->get_state_str(), exec->get_remaining());
+        }
+
+    }
+
+
+    XBT_INFO("-------------- Summary of executed schedule ------------------");
+    for (const auto& a : dag) {
+
+        if (const auto* exec = dynamic_cast<sg4::Exec*>(a.get())) {
+            XBT_INFO("[%f->%f] '%s' executed on %s", exec->get_start_time(), exec->get_finish_time(), exec->get_cname(),
+            exec->get_host()->get_cname());
+        }
+
+        if (const auto* comm = dynamic_cast<sg4::Comm*>(a.get())) {
+            XBT_INFO("[%f->%f] '%s' transferred from %s to %s", comm->get_start_time(), comm->get_finish_time(),
+            comm->get_cname(), comm->get_source()->get_cname(), comm->get_destination()->get_cname());
+        }
+    }
+
+    XBT_INFO("TOTAL TIME: %f", e.get_clock());
+
+    return 0;
 }
